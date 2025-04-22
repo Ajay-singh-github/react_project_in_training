affc7871c412c53b0be6562b3ca58bf4887e0983
Ajay-singh-github <ajaysikata@gmail.com>
2025-04-22 22:29:48 +0530

    22-04-2025

--- src/Components/HeaderComponet.jsx
+++ src/Components/HeaderComponet.jsx
@@ -7,15 +7,63 @@ import { CiUser } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
 import { FaChevronDown } from "react-icons/fa6";
 import { FaTv } from "react-icons/fa6";
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function HeaderComponent(props) {
-   var  defaultSearch = props?props.defaultSearch:false
-  console.log(props)
+  var navigate = useNavigate()
+  var defaultSearch = props ? props.defaultSearch : false
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const handleClick=(text)=>{
+    if(text=="Home")
+    {
+      navigate("/")
+    }
+  }
+
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Home', 'Offers', 'Profile', 'Cart','Logout'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton onClick={()=>handleClick(text)}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return <div className='nav'>
     <div className='logo-img'><img src="https://media.ipoji.com/ipo/images/swiggy-logo.png"></img></div>
+    <div className="threeline" onClick={toggleDrawer(true)}><MenuIcon /></div>
+
     <div className='list'>
       <ul>
-        <li><a href="#">Other  &nbsp;&nbsp;&nbsp;<span>Sasaram,Jamuhar...</span></a><FaChevronDown /></li>
+        <li className="other"><a href="#">Other  &nbsp;&nbsp;&nbsp;<span>Sasaram,Jamuhar...</span></a><FaChevronDown /></li>
 
         <li>
           <div className='icon'>
@@ -68,5 +116,10 @@ export default function HeaderComponent(props) {
 
       </ul>
     </div>
+
+    <Drawer open={open} onClose={toggleDrawer(false)}>
+     {DrawerList}
+      
+    </Drawer>
   </div>;
 }
