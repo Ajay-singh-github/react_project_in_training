b692a026d5f8c31e4c2e4a32e684eafb10535713
Ajay-singh-github <ajaysikata@gmail.com>
2025-05-16 22:31:39 +0530

    .

---
+++ src/Storage/RootReducer.jsx
@@ -0,0 +1,24 @@
+const initialState = { employeeData: {} }
+
+export default function RootReducer(state = initialState , action)
+{
+    switch(action.type)
+    {
+        case "ADD_EMPLOYEE":
+                state.employeeData[action.payload[0]] = action.payload[1]
+                console.log(state.employeeData)
+                return {employeeData:state.employeeData}
+    
+        case "DEL_EMPLOYEE":
+             delete state.employeeData[action.payload[0]]
+             return {employeeData:state.employeeData}
+                // delete state.employeeData.action.payload[0]
+             break
+
+        case "EDIT_EMPLOYEE":
+             state.employeeData[action.payload[0]] = action.payload[1]
+             return {employeeData:state.employeeData}
+        default:
+            return {employeeData:state.employeeData}
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render( <App />)
+// ----------redux---------------------
+import RootReducer from './Storage/RootReducer.jsx';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+
+
+
+var store = createStore(RootReducer)
+
+// -----------------redux------------------
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
+)
